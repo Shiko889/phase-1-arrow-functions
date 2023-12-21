fa321fe235891fae9bd2de3623c895eba31bb1f4
shiko <sarah.tharao@student.moringaschool.com>
2023-12-21 14:45:59 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+// Function expression called divide
+const divide = function(a, b) {
+    return a / b;
+  };
+// Arrow function called square
+const square = (num) => {
+    return num * num;
+  };  
+  let divide = require('./otherFile'); 
+  divide = function(a, b) {
+    return a / b; 
+  };
+  module.exports = divide;
+  
+  
+
+
+
+
